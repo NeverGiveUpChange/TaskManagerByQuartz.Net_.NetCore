57d1d1e429f4b0b08693bc0b9cb3dbb534f5b8fd
NeverGiveUpChange <13220105380@163.com>
2018-07-30 11:40:07 +0800

    隐藏发送邮箱账号

--- JobManager_Infrastructure/MailUtil/SmtpClientConfigurationInfo.cs
+++ JobManager_Infrastructure/MailUtil/SmtpClientConfigurationInfo.cs
@@ -7,9 +7,9 @@ namespace JobManager_Infrastructure.MailUtil
 {
     public class SmtpClientConfigurationInfo
     {
-        public static string Host { get { return "smtp.163.com"; } private set { } }
-        public static string UserName { get { return "strive_cl@163.com"; } private set { } }
-        public static string PassWord { get { return "1234567abcdefg"; } private set { } }
+        public static string Host { get { return "xxxx"; } private set { } }
+        public static string UserName { get { return "xxxxx"; } private set { } }
+        public static string PassWord { get { return "xxxx"; } private set { } }
         public static SmtpDeliveryMethod SmtpDeliveryMethod { get { return SmtpDeliveryMethod.Network; } private set { } }
         public static bool UseDefaultCredentials { get { return true; } private set { } }
     }
